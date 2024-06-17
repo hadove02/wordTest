5a62e90c8624ced7268b28c3dde2be00fe178abc
hadove02 <hadove02@naver.com>
2024-06-17 18:19:46 +0900

    시퀀스 파트 추가

--- src/Question.java
+++ src/Question.java
@@ -5,6 +5,71 @@ import java.util.Scanner;
 public class Question {
     final static int ANSWER = 0, QUESTION = 1;
 
+    public static void sequenceQuestion(ArrayList<String[]> data) {
+        ArrayList<String[]> wrongList = new ArrayList<>();
+        for (String[] sequence : data) {
+            if (!sequenceQuestionOne(sequence)) {
+                wrongList.add(sequence);
+            }
+        }
+
+        if (!wrongList.isEmpty()) {
+            sequenceQuestion(wrongList);
+        }
+    }
+
+    private static boolean sequenceQuestionOne(String[] sequence) {
+        Random rnd = new Random();
+        Scanner sc = new Scanner(System.in);
+        boolean[] rndIndex = new boolean[sequence.length];
+        boolean isCorrect = true;
+
+        System.out.println(sequence[0]);
+
+        System.out.print("랜덤 개수 선택 (최대 개수 : " + (sequence.length - 1) + ")> ");
+        int rndAmount = Integer.parseInt(new Scanner(System.in).nextLine());
+
+
+        if (rndAmount > sequence.length - 1) {
+            System.out.print("랜덤 개수 다시 선택 (최대 개수 : " + (sequence.length - 1) + ")> ");
+            rndAmount = Integer.parseInt(sc.nextLine());
+        }
+
+        for (int i = 0; i < rndAmount; ) {
+            int index = rnd.nextInt(sequence.length - 1) + 1;
+            if (!rndIndex[index]) {
+                rndIndex[index] = true;
+                i++;
+            }
+        }
+
+        for (int i = 1; i < sequence.length; i++) {
+            if (!rndIndex[i]) {
+                System.out.print(" " + sequence[i] + " ");
+            } else {
+                System.out.print(" [     ] ");
+            }
+        }
+        System.out.println();
+
+        int index = 0;
+        for (int i = 0; i < rndAmount; i++) {
+            System.out.print((i + 1) + " > ");
+            String answer = sc.nextLine();
+            while (!rndIndex[index]) {
+                index++;
+            }
+            if (sequence[index].equals(answer)) {
+                System.out.println("정답");
+            } else {
+                System.out.println(sequence[index]);
+                isCorrect = false;
+            }
+            index++;
+        }
+        return isCorrect;
+    }
+
     public static void run(ArrayList<String[]> wordList) {
         while (!wordList.isEmpty()) {
             wordList = randomQuestion(wordList);
